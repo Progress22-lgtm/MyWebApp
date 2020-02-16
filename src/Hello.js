01a576a49264bd1783c99375d5f1875df52af73d
Progress22-lgtm <58606640+Progress22-lgtm@users.noreply.github.com>
2020-02-16 23:18:06 +0100

    Devmind commit in 2020-02-16 19:18:06

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Delight(){
+    return (
+        <div className="mainContainer">
+            <h1>Hello My Name is Delight and I am a new Pro Programmer</h1>
+        </div>    
+    )
+}
