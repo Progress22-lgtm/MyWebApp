05830c5c02eebc7c03f0674d7b981217bbc4d355
Progress22-lgtm <58606640+Progress22-lgtm@users.noreply.github.com>
2020-02-16 23:19:43 +0100

    Imported css in Delight object

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Hello.css";
 
 function Delight(){
     return (
         <div className="mainContainer">
             <h1>Hello My Name is Delight and I am a new Pro Programmer</h1>
         </div>    
-    )
+    );
 }
 
 export default Delight;
